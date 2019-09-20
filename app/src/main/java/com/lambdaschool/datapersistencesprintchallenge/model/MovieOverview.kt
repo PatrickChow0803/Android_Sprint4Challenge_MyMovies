73c57271a7fe291d99018931c38f34a176ea4d1c
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-20 14:47:52 -0400

    Added a MovieDao

--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/model/MovieOverview.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/model/MovieOverview.kt
@@ -1,9 +1,13 @@
 package com.lambdaschool.sprint4challenge_mymovies.model
 
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
+
 data class MovieSearchResult(val results: List<MovieOverview>)
 
+@Entity(tableName = "movieTable")
 data class MovieOverview(
-        val id: Int,
         val original_language: String,
         val original_title: String,
         val overview: String,
@@ -13,5 +17,10 @@ data class MovieOverview(
         val title: String,
         val isVideo: Boolean,
         val vote_average: Float,
-        val vote_count: Int
+        val vote_count: Int,
+
+        var favorited: Boolean = false,
+
+        @PrimaryKey(autoGenerate = true)
+        val id: Int
 )
