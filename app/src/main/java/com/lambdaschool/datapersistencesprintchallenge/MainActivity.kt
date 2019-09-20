bc6ef145c4adad2cc5b6d2124c6e84f7ec57ff37
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-20 14:56:04 -0400

    Added MovieAPI

--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.lambdaschool.datapersistencesprintchallenge
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_fav.setOnClickListener {
+            startActivity(Intent(this, FavoritesActivity::class.java))
+        }
     }
 }
